06622b3d815d1a310847681e632434ab2432268b
gmalicki@gmail.com <gmalicki@gmail.com>
2009-04-22 17:55:44 -0400

    yes

--- rebooter.rb
+++ rebooter.rb
@@ -1,8 +1,21 @@
 require 'rubygems'
 require 'fileutils'
 
+BASE_PATH = "/home/webrentals/webroot/"
+DELAY = 200
+
+def reboot_on
+  FileUtils.cp BASE_PATH+ 'should_on.php', BASE_PATH+ 'should.php' 
+end
+
+def reboot_off
+  FileUtils.cp BASE_PATH+ 'should_off.php', BASE_PATH+'should.php'
+end
+
 loop do
-  sleep 100
-  reboot
-  sleep 10
+  reboot_on
+  sleep 20
+  reboot_off
+  sleep DELAY
+  puts "rebooting"
 end
